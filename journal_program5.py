dac06eb431bbc11656fc5b4538e2e9a150d5bd62
rutika-bulabule <rutikabulabule@gmail.com>
2025-11-28 14:04:23 +0530

    Saving Fail

--- journal_program5.py
+++ journal_program5.py
@@ -1,13 +1,11 @@
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: python simple_interest.py <principal> <rate> <time>")
+if len(sys.argv) < 4:
+    print("Usage: python journal_program5.py <P> <R> <T>")
     sys.exit(1)
 
 P = float(sys.argv[1])
 R = float(sys.argv[2])
 T = float(sys.argv[3])
-
-si = (P * R * T) / 100
-
-print("Simple Interest:", si)
+SI = (P * R * T) / 100
+print("Simple Interest:", SI)
